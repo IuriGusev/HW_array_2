1192440f3ef26d43c18a4d65137e49ea1f700633
iurii_gusev <gusevyuv@gmail.com>
2023-05-27 13:44:44 -0400

    HW_array_2 was made

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,80 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1(){
+        //Бухгалтеры попросили посчитать сумму всех выплат за месяц.
+        //Напишите программу, которая решит эту задачу, и выведите в консоль результат в формате:
+        // «Сумма трат за месяц составила … рублей».
+        System.out.println("task1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+    }
+    public static void task2(){
+        //Также бухгалтерия попросила найти минимальную и максимальную трату за день.
+        //Напишите программу, которая решит эту задачу, и выведите в консоль результат в формате:
+        // «Минимальная сумма трат за день составила … рублей. Максимальная сумма трат за день составила … рублей».
+        System.out.println("task2");
+        int[] arr = generateRandomArray();
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            for (int j =30; j < arr.length; j--) {
+                if (arr[j] > min) {
+                    min=arr[j];
+                }
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min  + " рублей. " +
+                "Максимальная сумма трат за день составила " + max + " рублей");
+    }
+    public static void task3(){
+        //Теперь бухгалтерия хочет понять, какую в среднем сумму компания тратила в течение 30 дней.
+        //Напишите программу, которая посчитает среднее значение трат за месяц (то есть сумму всех трат за месяц
+        // поделить на количество дней), и выведите в консоль результат в формате: «Средняя сумма трат за месяц составила … рублей».
+        //Важно помнить: подсчет среднего значения может иметь остаток, то есть быть не целым, а дробным числом.
+        System.out.println("task3");
+        int[] arr = generateRandomArray();
+        double average = 0;
+        if (arr.length > 0)
+        {
+            double sum = 0;
+            for (int j = 0; j < arr.length; j++) {
+                sum += arr[j];
+            }
+            average = sum / arr.length;
+        }
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
+    }
+    public static void task4(){
+        //В бухгалтерской книге появился баг. Что-то пошло нет так — фамилии и имена сотрудников начали отображаться
+        // в обратную сторону. Т. е. вместо «Иванов Иван» мы имеем «навИ вонавИ». Данные с именами сотрудников хранятся
+        // в виде массива символов — char[ ].
+        //Напишите код, который в случае такого бага будет выводить фамилии и имена сотрудников в корректном виде.
+        // В результате в консоль должно быть выведено: Ivanov Ivan.
+        System.out.println("task4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
